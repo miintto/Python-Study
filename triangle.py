9423e6b72d6338b325f2b78358e7deaeed9503ae
miintto <minjp93@gmail.com>
2019-01-07 22:45:26 +0900

    [Project] 삼각형 class로 작성

--- triangle.py
+++ triangle.py
@@ -4,46 +4,67 @@
 import numpy as np
 
 
-def innr_prd(vec1, vec2):
-    '''3차원 상의 두 벡터를 입력하면 두 벡터의 내적을 계산'''
-    return sum([vec1[i]*vec2[i] for i in range(3)])
-
-
-def crss_prd(vec1, vec2):
-    '''3차원 상의 두 벡터를 입력하면 두 벡터의 외적을 계산'''
-    i = vec1[1]*vec2[2] - vec1[2]*vec2[1]
-    j = vec1[2]*vec2[0] - vec1[0]*vec2[2]
-    k = vec1[0]*vec2[1] - vec1[1]*vec2[0]
-
-    return [i, j, k]
-
-
-def form_triangle(pt1, pt2, pt3):
+class Triangle():
     '''
     3차원 상의 점 3개를 입력하면
     그 점이 이루는 평면의 법선벡터를 추가하여
     삼각형 오브젝트를 출력
     '''
-    x = [pt1[i]-pt2[i] for i in range(3)]
-    y = [pt1[i]-pt3[i] for i in range(3)]
+    def __init__(self, point1, point2, point3):
+        self.point1 = point1
+        self.point2 = point2
+        self.point3 = point3
+        self.set_normal_vector()
 
-    return    [pt1, pt2, pt3, crss_prd(x, y)]
+    def __repr__(self):
+        pt1 = str(self.point1)
+        pt2 = str(self.point2)
+        pt3 = str(self.point3)
+        normal_vector = str(self.normal_vector)
+        return '['+pt1+', '+pt2+', '+pt3+'], normal vector = '+normal_vector
 
+    def set_normal_vector(self):
+        '''
+        삼각형이 포함한 평면의 법선벡터를 계산
+        '''
+        x = [self.point1[i]-self.point2[i] for i in range(3)]
+        y = [self.point1[i]-self.point3[i] for i in range(3)]
+        self.normal_vector = list(np.cross(x, y))
 
-def proj_pt(point, vec, triangle):
-    '''
-    특정한 점을 주어진 벡터에 대하여
-    삼각형이 포함된 평면으로 정사영
-    평면과 벡터가 서로 평행이면 None을 반환
-    '''
-    normal_vec = triangle[3]
-    if innr_prd(normal_vec, vec) == 0:
-        raise ValueError('정사영시킬 수 없음. (벡터와 평면이 서로 평행)')
-    else:
-        P_vec = [triangle[0][i]-point[i] for i in range(3)]
-        t = innr_prd(normal_vec, P_vec) / innr_prd(normal_vec, vec)
-        x, y, z = [point[i]+vec[i]*t for i in range(3)]
-        return [x, y, z]
+    def area(self):
+        '''
+        세 점이 주어지면 삼각형의 넓이 계산
+        '''
+        vec1 = [self.point1[i]-self.point2[i] for i in range(3)]
+        vec2 = [self.point1[i]-self.point3[i] for i in range(3)]
+        crss_prd_vec = [i**2 for i in np.cross(vec1, vec2)]
+        return np.sqrt(sum(crss_prd_vec))/2
+
+    def reverse_vector(self):
+        '''
+        법선벡터의 방향을 반대로 바꿈
+        '''
+        self.normal_vector.x = -self.normal_vector.x
+        self.normal_vector.y = -self.normal_vector.y
+        self.normal_vector.z = -self.normal_vector.z
+
+    def project(self, vector, triangle):
+        '''
+        주어진 벡터에 대하여 삼각형이 포함된 평면으로 정사영
+        평면과 벡터가 서로 평행이면 None을 반환
+        '''
+        normal_vec_plane = triangle.normal_vector
+        if np.dot(normal_vec_plane, vector) == 0:
+            raise ValueError('정사영시킬 수 없음. (벡터와 평면이 서로 평행)')
+        else:
+            projected_pt = []
+            for pt in [self.point1, self.point2, self.point3]:
+                P_vec = [triangle.point1[i]-pt[i] for i in range(3)]
+                t = np.dot(normal_vec_plane, P_vec) / np.dot(normal_vec_plane, vector)
+                x, y, z = [pt[i]+vector[i]*t for i in range(3)]
+                projected_pt.append([x, y, z])
+            return projected_pt
+
 
 
 def is_in_triangle(point, triangle):
@@ -70,35 +91,18 @@ def is_in_triangle(point, triangle):
         return True
 
 
-def area_triangle(triangle):
-    '''세 점이 주어지면 삼각형의 넓이 계산'''
-    pt1, pt2, pt3, normal_vec = triangle
-    vec1 = [pt1[i]-pt2[i] for i in range(3)]
-    vec2 = [pt1[i]-pt3[i] for i in range(3)]
-    crss_prd_vec = [i**2 for i in crss_prd(vec1, vec2)]
-    return np.sqrt(sum(crss_prd_vec))/2
 
 
-
-T1 = form_triangle([3, 0, 0], [0, 4, 0], [0, -1, 0])
-T2 = form_triangle([2, 2, 3], [2, 0, 5], [-3, 0, 7])
-print('triangle1 : %r' % T1)
-print('triangle2 : %r' % T2)
+T1 = Triangle([3, 0, 0], [0, 4, 0], [0, -1, 0])
+T2 = Triangle([2, 2, 3], [2, 0, 5], [-3, 0, 7])
+print('triangle1 :', T1)
+print('triangle2 :', T2)
 print()
 
-light = [0, 1, -1]
-
-T3 = []
-print('light : ', light)
-print('삼각형 T1을 T2가 포함된 평면으로 정사영')
-for point in T1[:-1]:
-    projected_pt = proj_pt(point, light, T2)
-    print('%r -> %r' % (point, projected_pt))
-    print('In triangle :', is_in_triangle(projected_pt, T2))
-    T3.append(projected_pt)
-
-T3 = form_triangle(T3[0], T3[1], T3[2])
-print('New Triangle :', T3)
-print('T1 의 넓이 :', area_triangle(T1))
-print('T2 의 넓이 :', area_triangle(T2))
-print('T3 의 넓이 :', area_triangle(T3))
+light = [0, 0, -1]
+x, y, z = T2.project(light, T1)
+T3 = Triangle(x, y, z)
+print(T3)
+print(T1.area())
+print(T2.area())
+print(T3.area())
