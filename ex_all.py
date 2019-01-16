78a200d05c797beed7327c85eb5ac2364577fb06
miintto <minjp93@gmail.com>
2019-01-16 13:38:18 +0900

    [2019.01/06] Day8

--- ex_all.py
+++ ex_all.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# 2019.01/14
+# 2019.01/14 ~ 15
 
 
 class Complex:
@@ -23,16 +23,17 @@ class Complex:
         y = self.re*z.im + self.im*z.re
         return Complex(x, y)
 
-    def __str__(self):   ###  ___repr__ 과의 차이점은?
+    def __str__(self):   ### __repr__ 과의 차이점은?
+        '''print 함수와 연관'''
         if self.im < 0:
             sgn = ' - '
             y = -self.im
         else:
             sgn = ' + '
             y = self.im
-        return '{0}{1}{2}i'.format(self.re, sgn, y)
+        return '{0}{1}{2}i'.format(self.re, sgn, y)   ### {}안에는
 
-    def show(self, arg):
+    def show(self, arg = ''):
         if self.im < 0:
             sgn = ' - '
             y = -self.im
@@ -53,6 +54,11 @@ class Vector:
             self.vec.append(float(val))
             cnt += 1
         self.dim = cnt
+
+    def __str__(self):
+        vec = str(self.vec)
+        vec = vec.replace('[', '< ').replace(']', ' >').replace(',', ' ,')
+        return vec
 
     def SetVector(self, src):
         self.dim = len(src)
@@ -85,7 +91,7 @@ class Triangle:
         try:
             self.vertex[offset] = val.vec
         except ValueError:
-            print('No $d-th vertex' % offset)
+            print('No %d-th vertex' % offset)
         return self
 
     def SetNormal(self, n=None):
@@ -93,7 +99,8 @@ class Triangle:
             self.normal = n
         else:
             pass
-#            self.normal = Cross()    미완...
+            #self.normal = Cross()    미완...
+        return self
 
 
 class MyTriagle(Triangle):   ### 상속 클래스
@@ -104,21 +111,36 @@ class MyTriagle(Triangle):   ### 상속 클래스
         v2 = v2_vec.GetVector()
         n = n_vec.GetVector()
         super(MyTriagle, self).__init__(v0, v1, v2, n)
-        ### 상위 클래스를 불러서 해당 변수를 집어넣어준다.
-        ### super()안의 빈칸을 비워놔도 상관없는듯...
+            '''
+            상위 클래스를 불러서 해당 변수를 집어넣어준다.
+            super()안의 빈칸을 비워놔도 상관없는듯...
+            '''
+
+    def SetNormal(self, n1, n2, n3):
+        '''
+        상속받은 클래스에서 부모 클래스에 있는 함수와 똑같은 이름의 함수를 만들면
+        상속받은 클래스의 함수가 우선적으로 적용, 부모 클래스의 함수는 작동하지 않음
+        -> 오버라이딩
+        '''
+        self.normal = [n1, n2, n3]
+        return self
+
+
 
 print('name', __name__=='__main__')
 
 if __name__ == '__main__':
-### 실행한 모듈이 메인 프로그램인지 확인하는 작업
-### 여기서 __name__은 자신을 포함하고 있는 모듈의 이름을 가리킨다.
-### 예를들어 커멘드에서 python3 ex_all.py 를 실행해보면 
-### __name__에는 'ex_all'라는 이름이 메인으로 지정된다.
-### 그래서 __name__=='__main__'는 True라는 값을 반환하게된다.
+    '''
+    실행한 모듈이 메인 프로그램인지 확인하는 작업
+    여기서 __name__은 자신을 포함하고 있는 모듈의 이름을 가리킨다.
+    예를들어 커멘드에서 python3 ex_all.py 를 실행해보면
+    __name__에는 'ex_all'라는 이름이 메인으로 지정된다.
+    그래서 __name__=='__main__'는 True라는 값을 반환하게된다.
 
-### 하지만 ex_all_2.py 라는 모듈을 만들어서 ex_all을 import하여 ex_all_2.py를 실행하는 경우에는
-### __name__에는 ex_all_2라는 값이 들어가게 되어
-### __name__=='__main__'는 False 를 반환하게 된다.
+    하지만 ex_all_2.py 라는 모듈을 만들어서 ex_all을 import하여 ex_all_2.py를 실행하는 경우에는
+    __name__에는 ex_all_2라는 값이 들어가게 되어
+    __name__=='__main__'는 False 를 반환하게 된다.
+    '''
     print('It Work')
     a = Complex(1, 2)
     a.show('a = ')
@@ -130,18 +152,22 @@ if __name__ == '__main__':
     y1.show('a add to b : ')
     y2.show('a + b : ')
     y3 = a*b
-    y3.show('a * b = ')
-    print(y3)
+    y3.show('a * b =')
+    print('a * b =', y3, end='\n\n')
 ### 여기까지의 내용은 ex_all을 직접 실행하는 경우에만 실행이 된다.
 ### 이런 작업은 debugging때 주로 사용할 수 있다.
 
 
 v1 = Vector(1, 2, 3)
 v1.show('v1 =')
+print('v1 :', v1)
 
 v2 = Vector().SetVector([1, -1, 0]).show('v2 =')   #1 이렇게 인스턴스를 return받으면 SetVector와 show를 동시에 작성할 수 있다. 함수를 한번에 쭉 이어서 작성할 수 있음
 v3 = Vector().SetVector([1, 1, 0]).show('v3 =')
 v4 = Vector().SetVector([0, 1, -1]).show('v4 =')
 
-T1 = Triangle(v1.GetVector(), v2.GetVector(), v3.GetVector(), v4.GetVector()).show('Triangle : ')
-T2 = MyTriagle(v1, v2, v3, v4).show('MyMy')
+T1 = Triangle(v1.GetVector(), v2.GetVector(), v3.GetVector(), v4.GetVector())
+T1.SetNormal([-1, -2, -3]).show('T1 : ')
+
+T2 = MyTriagle(v1, v2, v3, v4)
+T2.SetNormal(-1, -2, -3).show('T2 : ')
