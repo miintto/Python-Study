10ec73a9774fdc9c2a909d7b5ba13e57bf386b7f
miintto <minjp93@gmail.com>
2019-02-11 22:00:38 +0900

    [2019.02/11] Day10 공부

--- ex_decorator.py
+++ ex_decorator.py
@@ -7,59 +7,54 @@ import sys, math, functools
 def power(n):
 
     def compute(x):
-        prod = 1;
-        for k in range(n): prod *= x;
-        return prod;
+        prod = 1
+        for _ in range(n):
+            prod *= x
+        return prod
 
-    return compute;
-    # return 함수 가능
+    return compute   ### 함수를 return 가능
 
-def do_this(fun):
-    val = fun(4);
-    return val;
+
+def do_this(fun):   ### 함수를 받아서
+    val = fun(4)   ### 함수에 4 대입 (대신 4를 input으로 받을수 있는 fun만 가능)
+    return val
+
 
 def do_this_smart(fun):
-    def running(*ags, **kws):
-        val = fun(*ags, **kws);
-        return val;
-    return running;
+    def running(*ags, **kws):   ### 이렇게 여러 인자를 받아서
+        val = fun(*ags, **kws)   ### 그걸 계산하고
+        return val   ### 결과를 출력
+    return running   ### 하는 함수를 출력  (사실살 그냥 fun()과 똑같은 기능...)
+
 
 def fun_1(x):
-    return x * x;
+    return x * x
 
 def fun_2(x, y):
-    return x * y;
+    return x * y
+
+
+def setunit(unit):
+    def reflecting(fun):
+        fun.unit = unit   ### def 밖에서 함수에 변수를 지정해줄 수 있음
+        return fun
+    return reflecting
+
 
 # decorators
 
 def decoration(fun):
-
-    '''decoration house'''
-    # print(decoration.__doc__); -> 함수안에 있는 script가 출력됨
+    '''decoration house'''   ### Docstring : decoration.__doc__ 으로 출력
 
     @functools.wraps(fun)
     def wrapping_fun(*ags, **kvs):
-        print('wrapping-pre');
-        rtv = fun(*ags, **kvs);
-        print('wrapping-post');
-        return rtv;
+        print('<< wrapping-pre >>')
+        rtv = fun(*ags, **kvs)
+        print('<< wrapping-post >>')
+        return rtv
 
-    return wrapping_fun;
+    return wrapping_fun
 
-def setunit(unit):
-    def reflecting(fun):
-        fun.unit = unit;
-        # def 밖에서 함수에 변수를 지정해줄 수 있음
-        return fun;
-    return reflecting;
-
-def sample_1(x, y):
-    print('args in sample_1 = ', x, y);
-    return x + y;
-
-def sample_2(x, y, z):
-    print('args in sample_2 = ', x, y, z);
-    return x * y * z;
 
 @decoration
 # another가 decoration이랑 같이 실행됨
@@ -68,30 +63,40 @@ def sample_2(x, y, z):
 # 한 줄마다 @로 여러개의 decoration이 가능함.
 # 밑에 있는 decoration부터 실행됨
 def another(x, y, z):
-    print('args in another function = ', x, y, z);
-    return x + y + z;
+    print('더할 숫자들은 = ', x, y, z)
+    return x + y + z
+
+def sample_1(x, y, z):
+    print('더할 숫자들은 : ', x, y, z)
+    return x + y + z
+
+def sample_2(x, y, z):
+    print('곱할 숫자들은 = ', x, y, z)
+    return x * y * z
 
 @setunit('cm')
 def sample_3(x, y, z):
-    return x * y * z;
+    return x * y * z
 
 if __name__ == '__main__':
-    pw = power(3);
-    print(pw(2));
+    pw = power(3);   ###  pw(x) = x^3
+    print('pw(2) =', pw(2))
 
-    print(do_this(fun_1));
+    print('do_this(fun_1) =', do_this(fun_1))   ### 4*4
 
-    myfun = do_this_smart(fun_2);
-    print(myfun(4, 5), '\n');
+    myfun = do_this_smart(fun_2)
+    print(myfun(4, 5))
+    print(fun_2(4, 5), '\n')   ### 결과가 같다.
+
 
 # decoration test
 
-    sample_1 = decoration(sample_1);
-    print(sample_1(1, 2), '\n');
+    print(another(1, 2, 3), '\n')
 
-    print(another(1, 2, 3), '\n');
+    sample_1 = decoration(sample_1)
+    print(sample_1(1, 2, 3), '\n')   ### 얘랑 같은 효과임
 
-    sample_2 = setunit('cm')(sample_2);
-    print(sample_2(3, 4, 5), sample_2.unit, '\n');
+    sample_2 = setunit('cm')(sample_2)
+    print(sample_2(3, 4, 5), sample_2.unit)
 
-    print(sample_3(3, 4, 5), sample_3.unit);
+    print(sample_3(3, 4, 5), sample_3.unit, '\n')
